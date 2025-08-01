c2bcea333c7f47dbd4057fec04b1c1fa46b5dd1b
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-01 17:39:39 +0600

    Add collected static files for production

--- Back_end/school_backend/settings.py
+++ Back_end/school_backend/settings.py
@@ -159,6 +159,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
