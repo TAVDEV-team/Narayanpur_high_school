b71dca904b2126d309758229d7351ee409c5d066
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-02 15:03:15 +0600

    addeded the data base

--- Back_end/school_backend/settings.py
+++ Back_end/school_backend/settings.py
@@ -108,14 +108,19 @@ REST_FRAMEWORK = {
 
 
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT"),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
