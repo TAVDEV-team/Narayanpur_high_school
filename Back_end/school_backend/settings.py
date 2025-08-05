89a19a7498a91f3400682f55635b29ba80b9bb62
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-05 16:13:49 +0600

    postgres

--- Back_end/school_backend/settings.py
+++ Back_end/school_backend/settings.py
@@ -111,14 +111,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config("DATABASE_NAME"),
         'USER': config("DATABASE_USER"),
-        'PASSWORD': config("DATABASE_PASSWORD"),
-        'HOST': config("DATABASE_HOST"),
-        'PORT': config("DATABASE_PORT"),
+        'PASSWORD': config("DATABASE_PASSWORD", default=""),
+        'HOST': config("DATABASE_HOST", default="localhost"),
+        'PORT': config("DATABASE_PORT", default="5432"),
     }
 }
 # Password validation
