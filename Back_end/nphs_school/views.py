c8fa8b2f37cd4ea84d42fc51ea4fc4f99d03d4e3
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-15 11:49:05 +0600

    api created to find students of that class

--- Back_end/nphs_school/views.py
+++ Back_end/nphs_school/views.py
@@ -3,7 +3,9 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
+from accounts.models import StudentAccount
 from accounts.permissions import IsHeadMaster, IsTeacher
+from accounts.serializers import StudentSerializer
 from nphs_school.models import About, AClass, Batch, Notice, School, Subject
 from nphs_school.serializers import (
     AboutSerializer,
@@ -41,6 +43,19 @@ class SchoolViewSet(viewsets.ModelViewSet):
 class AClassViewSet(viewsets.ModelViewSet):
     queryset = AClass.objects.all()
     serializer_class = AClassSerializer
+
+    @action(detail=True, methods=["get"], url_path="detail")
+    def get_detail(self, request, pk=None):
+        aclass = self.get_object()
+        students = StudentAccount.objects.filter(batch__current_class=aclass)
+        student_serializer = StudentSerializer(students, many=True)
+
+        return Response(
+            {
+                "aclass": self.get_serializer(aclass).data,
+                "students": student_serializer.data,
+            }
+        )
 
 
 class BatchViewSet(viewsets.ModelViewSet):
