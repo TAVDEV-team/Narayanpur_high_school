da4a4040fb9f2601b3c781804c21b0ba36c64fcb
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-16 14:55:03 +0600

    allow all

--- Back_end/fund/views.py
+++ Back_end/fund/views.py
@@ -2,6 +2,7 @@ from rest_framework import status, viewsets
 # from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from rest_framework.permissions import AllowAny
 # from accounts.permissions import IsHeadMaster
 from fund.models import Fund, FundTransaction
 from fund.serializers import FundSerializer, FundTransactionSerializer
@@ -10,12 +11,14 @@ from fund.serializers import FundSerializer, FundTransactionSerializer
 class FundViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Fund.objects.all()
     serializer_class = FundSerializer
+    permission_classes = [AllowAny]
     # permission_classes = [IsAuthenticated, IsHeadMaster]
 
 
 class FundTransactionViewSet(viewsets.ModelViewSet):
     queryset = FundTransaction.objects.all()
     serializer_class = FundTransactionSerializer
+    permission_classes = [AllowAny]
     # permission_classes = [IsAuthenticated, IsHeadMaster]
 
     def update(self, request, *args, **kwargs):
