09d86e32c6e379520eec3b584355304484e7d299
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-22 09:21:54 +0600

    solved class problem

--- Back_end/accounts/serializers/student_serializer.py
+++ Back_end/accounts/serializers/student_serializer.py
@@ -2,45 +2,50 @@ from django.db import transaction
 from rest_framework import serializers
 
 from accounts.models import StudentAccount
+from nphs_school.models import AClass
 
 from .account_serializer import AccountSerializer
 
 
 class StudentSerializer(serializers.ModelSerializer):
     account = AccountSerializer()
-    aclass = serializers.CharField(source="batch.current_class")
+    aclass_id = serializers.PrimaryKeyRelatedField(
+        queryset=AClass.objects.all(), write_only=True
+    )
+    aclass = serializers.CharField(source="batch.aclass.name", read_only=True)
     batch_label = serializers.CharField(source="batch.label", read_only=True)
 
     class Meta:
         model = StudentAccount
         fields = [
-            "aclass",
+            "aclass_id",  # client provides this
+            "aclass",  # read-only name
             "batch_label",
             "group",
             "roll_number",
             "account",
         ]
-        read_only_fields = ["roll_number"]
+        read_only_fields = ["roll_number", "aclass", "batch_label"]
 
     def create(self, validated_data):
         account_data = validated_data.pop("account")
+        aclass = validated_data.pop("aclass_id")
 
         with transaction.atomic():
+            # create account first
             account_serializer = AccountSerializer(data=account_data)
             account_serializer.is_valid(raise_exception=True)
             account = account_serializer.save()
+
+            # resolve batch from class
+            batch = aclass.batch
+            if not batch:
+                raise serializers.ValidationError(
+                    "This class has no batch assigned."
+                )
+
             student = StudentAccount.objects.create(
-                account=account, **validated_data
+                account=account, batch=batch, **validated_data
             )
+
         return student
-
-    def update(self, instance, validated_data):
-        account_data = validated_data.pop("account", None)
-        if account_data:
-            account_serializer = AccountSerializer(
-                instance=instance.account, data=account_data, partial=True
-            )
-            account_serializer.is_valid(raise_exception=True)
-            account_serializer.save()
-
-        return super().update(instance, validated_data)
