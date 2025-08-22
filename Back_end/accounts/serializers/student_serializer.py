a8cfb583eb35e0ae6044a1ea48bb541b9c4a7063
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-22 20:47:24 +0600

    added full student registration system

--- Back_end/accounts/serializers/student_serializer.py
+++ Back_end/accounts/serializers/student_serializer.py
@@ -8,17 +8,19 @@ from .account_serializer import AccountSerializer
 
 
 class StudentSerializer(serializers.ModelSerializer):
+
     account = AccountSerializer()
-    aclass_id = serializers.PrimaryKeyRelatedField(
-        queryset=AClass.objects.all(), write_only=True
-    )
+    class_name = serializers.CharField(write_only=True)
+    group = serializers.CharField(write_only=True)
+
     aclass = serializers.CharField(source="class", read_only=True)
     batch_label = serializers.CharField(source="batch.label", read_only=True)
 
     class Meta:
         model = StudentAccount
         fields = [
-            "aclass_id",
+            "class_name",
+            "group",
             "aclass",
             "batch_label",
             "group",
@@ -29,7 +31,25 @@ class StudentSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         account_data = validated_data.pop("account")
-        aclass = validated_data.pop("aclass_id")
+        aclass_id = validated_data.pop("class_name")
+        group_map = {
+            "9": {
+                "science": "9_science",
+                "business": "9_business",
+                "humanities": "9_humanities",
+            },
+            "10": {
+                "science": "10_science",
+                "business": "10_business",
+                "humanities": "10_humanities",
+            },
+        }
+        if aclass_id in group_map:
+            aclass_name = group_map[aclass_id].get(validated_data["group"])
+            print(aclass_name)
+        else:
+            aclass_name = aclass_id
+        aclass = AClass.objects.get(name=aclass_name)
 
         with transaction.atomic():
             last_roll = (
@@ -41,6 +61,10 @@ class StudentSerializer(serializers.ModelSerializer):
             number = (last_roll.roll_number + 1) if last_roll else 1
             user_name = f"{str(aclass.batch)}{number}"
             account_data['user']['username'] = user_name
+            email_dummy = account_data['user']['email']
+            if email_dummy == "dummy_email@frontend.com":
+                email_made = f"{user_name.lower()}@gmail.com"
+            account_data['user']['email'] = email_made
             # create account first
             account_serializer = AccountSerializer(data=account_data)
             account_serializer.is_valid(raise_exception=True)
