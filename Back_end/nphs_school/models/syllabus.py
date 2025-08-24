bb327b4cd423da2e432a3bb12d8dc178ab8802ce
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-24 23:57:19 +0600

    updated the serializer

--- Back_end/nphs_school/models/syllabus.py
+++ Back_end/nphs_school/models/syllabus.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Syllabus(models.Model):
     title = models.CharField(max_length=255)
+    aclass = models.OneToOneField(
+        'nphs_school.AClass', null=True, blank=True, on_delete=models.SET_NULL
+    )
     file = models.FileField(upload_to="syllabus/")
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
--- Back_end/nphs_school/serializers.py
+++ Back_end/nphs_school/serializers.py
@@ -204,6 +204,15 @@ class RoutineSerializer(serializers.ModelSerializer):
 
 
 class SyllabusSerializer(serializers.ModelSerializer):
+    class_title = serializers.CharField(source="aclass.name", read_only=True)
+
     class Meta:
         model = Syllabus
-        fields = ["id", "title", "file", "uploaded_at"]
+        fields = [
+            "id",
+            "aclass",
+            "class_title",
+            "title",
+            "file",
+            "uploaded_at",
+        ]
