bb9659b3c1d3932d1432bcd9c333cc4e2cd27bb1
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-25 16:20:01 +0600

    apply redis to list views

--- Back_end/accounts/views/student_view.py
+++ Back_end/accounts/views/student_view.py
@@ -1,15 +1,15 @@
-# from django.utils.decorators import method_decorator
-# from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.viewsets import ModelViewSet
 
 from accounts.models import StudentAccount
 from accounts.serializers import StudentSerializer
 
 
-# @method_decorator(cache_page(60 * 5), name="list")  # cache list view 5 mins
-# @method_decorator(
-#     cache_page(60 * 5), name="retrieve"
-# )  # cache detail view 5 mins
+@method_decorator(cache_page(60 * 5), name="list")  # cache list view 5 mins
+@method_decorator(
+    cache_page(60 * 5), name="retrieve"
+)  # cache detail view 5 mins
 class StudentAccountViewSet(ModelViewSet):
     queryset = StudentAccount.objects.all()
     serializer_class = StudentSerializer
--- Back_end/accounts/views/teacher_views.py
+++ Back_end/accounts/views/teacher_views.py
@@ -1,9 +1,13 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.viewsets import ModelViewSet
 
 from accounts.models import TeacherAccount
 from accounts.serializers import TeacherSerializer
 
 
+@method_decorator(cache_page(60 * 5), name="list")
+@method_decorator(cache_page(60 * 5), name="retrieve")
 class TeacherAccountViewSet(ModelViewSet):
     queryset = TeacherAccount.objects.all()
     serializer_class = TeacherSerializer
