7dc65172ae7aafd6c4e556998b76af8b88ec56fa
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-27 10:10:18 +0600

    authentication added for funds

--- Back_end/fund/views.py
+++ Back_end/fund/views.py
@@ -3,10 +3,10 @@ from django.views.decorators.cache import cache_page
 from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny
 
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-# from accounts.permissions import IsHeadMaster
+from accounts.permissions import IsHeadMaster
 from fund.models import Fund, FundTransaction
 from fund.serializers import FundSerializer, FundTransactionSerializer
 
@@ -15,19 +15,17 @@ class FundViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Fund.objects.all()
     serializer_class = FundSerializer
     permission_classes = [AllowAny]
-    # permission_classes = [IsAuthenticated, IsHeadMaster]
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsHeadMaster]
 
 
-@method_decorator(cache_page(60 * 5), name="list")  # cache list view 5 mins
+@method_decorator(cache_page(60 * 5), name="list")
 @method_decorator(
     cache_page(60 * 5), name="retrieve"
-)  # cache detail view 5 mins
+)
 class FundTransactionViewSet(viewsets.ModelViewSet):
     queryset = FundTransaction.objects.all()
     serializer_class = FundTransactionSerializer
-    permission_classes = [AllowAny]
-    # permission_classes = [IsAuthenticated, IsHeadMaster]
+    permission_classes = [IsAuthenticated, IsHeadMaster]
 
     def update(self, request, *args, **kwargs):
         return Response(
