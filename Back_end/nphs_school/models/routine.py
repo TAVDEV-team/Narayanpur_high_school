bc1ec682784d934a73465de4984076b22dcaacb3
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-27 21:50:53 +0600

    constraints updated in routine

--- Back_end/nphs_school/models/routine.py
+++ Back_end/nphs_school/models/routine.py
@@ -13,14 +13,14 @@ class Routine(models.Model):
     ]
 
     CLASS_SLOTS = [
-        ("1", "10:00 AM - 10:45 AM"),
-        ("2", "10:45 AM - 11:30 AM"),
-        ("3", "11:30 AM - 12:15 PM"),
-        ("4", "12:15 PM - 01:00 PM"),
-        ("5", "01:00 PM - 02:00 PM"),
-        ("6", "02:00 PM - 02:40 PM"),
-        ("7", "02:40 PM - 03:20 PM"),
-        ("8", "03:20 PM - 04:00 PM"),
+        (1, "10:00 AM - 10:45 AM"),
+        (2, "10:45 AM - 11:30 AM"),
+        (3, "11:30 AM - 12:15 PM"),
+        (4, "12:15 PM - 01:00 PM"),
+        (5, "01:00 PM - 02:00 PM"),
+        (6, "02:00 PM - 02:40 PM"),
+        (7, "02:40 PM - 03:20 PM"),
+        (8, "03:20 PM - 04:00 PM"),
     ]
 
     aclass = models.ForeignKey(
@@ -39,12 +39,16 @@ class Routine(models.Model):
     slot = models.CharField(max_length=2, choices=CLASS_SLOTS)
 
     class Meta:
-        ordering = ["aclass", "day", "slot"]
-        unique_together = (
-            "aclass",
-            "day",
-            "slot",
-        )
+        constraints = [
+            models.UniqueConstraint(
+                fields=["aclass", "day", "slot"],
+                name="unique_class_day_slot"
+            ),
+            models.UniqueConstraint(
+                fields=["teacher", "day", "slot"],
+                name="unique_teacher_day_slot"
+            ),
+        ]
 
     def __str__(self):
         return f"{self.aclass} - {self.get_day_display()} - {self.get_slot_display()} - {self.subject}"  # noqa: E501
