784227b9748ca59bc3055fc1309218b10b1d18a2
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-31 06:15:16 +0600

    added allowany to school views

--- Back_end/nphs_school/views/school_view.py
+++ Back_end/nphs_school/views/school_view.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from nphs_school.models import School
 from nphs_school.serializers import SchoolSerializer
@@ -7,3 +8,4 @@ from nphs_school.serializers import SchoolSerializer
 class SchoolViewSet(viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
+    permission_classes = [AllowAny]
