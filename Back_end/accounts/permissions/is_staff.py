147b14c041d6e3ce3de946a59b9400b1ecae5f01
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-12-25 20:33:40 +0000

    features #108: applied safe headmaster permission to teachers and staffs views

--- Back_end/accounts/permissions/is_staff.py
+++ Back_end/accounts/permissions/is_staff.py
@@ -22,31 +22,3 @@ class IsStaff(permissions.BasePermission):
                 or getattr(user, "is_headmaster", False)
             )
         )
-
-
-# from rest_framework import permissions
-# from accounts.models import OfficeHelpersAccount, TeacherAccount
-# import accounts.models.teacher import HeadMasterAccount
-
-
-# class IsStaff(permissions.BasePermission):
-#     """
-#     Allow everyone to read (GET/HEAD/OPTIONS).
-#     Only OfficeHelper, Teacher, or HeadMaster accounts can modify.
-#     """
-
-#     def has_permission(self, request, view):
-#         # SAFE methods = read-only
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
-
-#         if not request.user or not request.user.is_authenticated:
-#             return False
-
-#         user = request.user
-
-#         return (
-#             OfficeHelpersAccount.objects.filter(account__user=user).exists()
-#             or TeacherAccount.objects.filter(account__user=user).exists()
-#             or HeadmasterAccount.objects.filter(account__user=user).exists()
-#         )
